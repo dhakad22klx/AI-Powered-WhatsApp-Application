67a1036a76e175a3b726d1d946328b02e0e95de4
vikash1807 <vikashchahar2024@gmail.com>
2026-02-01 23:15:42 +0530

    code cleaning and remove deprecated code

--- start.py
+++ start.py
@@ -1,31 +1,48 @@
 import os
+from io import BytesIO
+
 import httpx
 import uvicorn
-from io import BytesIO
-from PIL import Image, ImageOps
-from fastapi import FastAPI, Request, Response, Query, BackgroundTasks
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
+from fastapi import (
+    BackgroundTasks,
+    Depends,
+    FastAPI,
+    Query,
+    Request,
+    Response
+)
 from pyngrok import ngrok
+from PIL import Image
+
 from chat_history import WhatsAppMemory
 
-load_dotenv()
 
 # --- CONFIGURATION ---
+load_dotenv()
 TOKEN = os.getenv("TOKEN")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 API_VERSION = os.getenv("API_VERSION", "v21.0")
 BASE_URL = f"https://graph.facebook.com/{API_VERSION}"
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    # Startup
+    app.state.http_client = httpx.AsyncClient()
+    yield
+
+    # Shutdown
+    await app.state.http_client.aclose()
+
+def get_http_client(request: Request) -> httpx.AsyncClient:
+    return request.app.state.http_client
+
+app = FastAPI(lifespan=lifespan)
 memory = WhatsAppMemory()
 
-# Create a global async client for better performance
-client = httpx.AsyncClient()
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await client.aclose()
 
 # --- WEBHOOK VERIFICATION ---
 @app.get("/webhook")
@@ -38,11 +55,18 @@ async def verify_webhook(
         return Response(content=hub_challenge, media_type="text/plain")
     return Response(content="Verification failed", status_code=403)
 
+
 # --- MAIN WEBHOOK RECEIVER ---
 @app.post("/webhook")
-async def receive_message(request: Request, background_tasks: BackgroundTasks):
-    data = await request.json()
+async def receive_message(
+    request: Request,
+    background_tasks: BackgroundTasks,
+    client: httpx.AsyncClient = Depends(get_http_client)
+):
     
+    data = await request.json()
+    print("data : ", data)
+
     try:
         value = data['entry'][0]['changes'][0]['value']
         if 'messages' in value:
@@ -54,10 +78,18 @@ async def receive_message(request: Request, background_tasks: BackgroundTasks):
             # 1. HANDLE TEXT
             if msg_type == 'text':
                 text_body = message['text']['body'].lower()
+
                 # Extract sender's profile name
-                print("")
-                sender_name = value['contacts'][0]['profile']['name'] if 'contacts' in value else "World"
-                background_tasks.add_task(handle_text_chat, sender_phone,sender_name, text_body, incoming_msg_id)
+                sender_name = value['contacts'][0]['profile']['name'] if 'contacts' in value else "Unknown"
+
+                background_tasks.add_task(
+                    handle_text_chat,
+                    client,
+                    sender_phone,
+                    sender_name,
+                    text_body, 
+                    incoming_msg_id
+                )
 
             # 2. HANDLE STICKER COMMAND (/s on an image)
             elif msg_type == 'image':
@@ -69,9 +101,16 @@ async def receive_message(request: Request, background_tasks: BackgroundTasks):
                     
                     pack_name = parts[0].strip() if len(parts) > 0 and parts[0] else "Creater"
                     publisher = parts[1].strip() if len(parts) > 1 else "Deepak Dhakad"
-
                     media_id = message['image']['id']
-                    background_tasks.add_task(handle_sticker_request, sender_phone, media_id, pack_name, publisher)
+
+                    background_tasks.add_task(
+                        handle_sticker_request,
+                        client,
+                        sender_phone,
+                        media_id,
+                        pack_name,
+                        publisher
+                    )
 
     except (KeyError, IndexError):
         pass
@@ -80,34 +119,55 @@ async def receive_message(request: Request, background_tasks: BackgroundTasks):
 
 # --- BACKGROUND LOGIC ---
 
-async def handle_text_chat(phone, text, sender_name, msg_id):
+async def handle_text_chat(
+    client : httpx.AsyncClient,
+    phone,
+    sender_name,
+    text,
+    msg_id
+):
     """Processes Text and sends text reply."""
     # Optional: Get context from memory
-    USE_CONTEXT = False  # <-- change to False to disable context
+    USE_CONTEXT = True  # <-- change to False to disable context
     context = memory.get_context(phone, text)
     reply_text = f"Got it, {sender_name}!. You said: {text}"
     if USE_CONTEXT:
         context = memory.get_context(phone, text)
         reply_text += f"\n(Relevant Informatoin found in chat in history: {context})"
     
-    await send_whatsapp_message(phone, reply_text, reply_to_id=msg_id)
+    await send_whatsapp_message(
+        client=client,
+        to_number=phone,
+        text=reply_text,
+        reply_to_id=msg_id
+    )
     memory.save_message(phone, text)
 
-async def handle_sticker_request(phone, media_id, pack_name = "Creater", publisher="Deepak Dhakad"):
+
+async def handle_sticker_request(
+    client : httpx.AsyncClient,
+    phone,
+    media_id,
+    pack_name = "Creater",
+    publisher="Deepak Dhakad"
+):
     """Processes image to sticker transformation."""
     try:
         # A. Get Download URL
         media_info_url = f"{BASE_URL}/{media_id}"
         headers = {"Authorization": f"Bearer {TOKEN}"}
-        resp = await client.get(media_info_url, headers=headers)
+        resp = await client.get(
+            media_info_url,
+            headers=headers
+        )
         download_url = resp.json().get("url")
-
         print(f"Received Download URL : {download_url}")
+
         # B. Download & Process Image
         img_resp = await client.get(download_url, headers=headers)
         img = Image.open(BytesIO(img_resp.content)).convert("RGBA")
-
         print(f"Processed Image")
+
         # Resize/Pad to exactly 512x512 transparent WebP - Sticker Canva
         img.thumbnail((512, 512), Image.Resampling.LANCZOS)
         sticker = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
@@ -123,10 +183,14 @@ async def handle_sticker_request(phone, media_id, pack_name = "Creater", publish
             "file": ("sticker.webp", webp_data, "image/webp"),
             "messaging_product": (None, "whatsapp"),
         }
-        upload_resp = await client.post(upload_url, headers=headers, files=files)
+        upload_resp = await client.post(
+            upload_url,
+            headers=headers, 
+            files=files
+        )
         new_media_id = upload_resp.json().get("id")
-
         print("Uploaded sticker to meta")
+
         # D. Send Sticker
         await client.post(
             f"{BASE_URL}/{PHONE_NUMBER_ID}/messages",
@@ -138,12 +202,17 @@ async def handle_sticker_request(phone, media_id, pack_name = "Creater", publish
                 "sticker": {"id": new_media_id}
             }
         )
-
         print("Sent requested sticker")
+
     except Exception as e:
         print(f"Sticker Error: {e}")
 
-async def send_whatsapp_message(to_number, text, reply_to_id=""):
+async def send_whatsapp_message(
+    client : httpx.AsyncClient,
+    to_number,
+    text,
+    reply_to_id=""
+):
     """Async text sender."""
     headers = {"Authorization": f"Bearer {TOKEN}"}
     payload = {
@@ -155,12 +224,27 @@ async def send_whatsapp_message(to_number, text, reply_to_id=""):
     if reply_to_id:
         payload["context"] = {"message_id": reply_to_id}
     
-    await client.post(f"{BASE_URL}/{PHONE_NUMBER_ID}/messages", headers=headers, json=payload)
+    await client.post(
+        f"{BASE_URL}/{PHONE_NUMBER_ID}/messages",
+        headers=headers,
+        json=payload
+    )
+
+
+# if __name__ == "__main__":
+#     PORT = 8000
+
+#     if os.getenv("NGROK_AUTH_TOKEN"):
+#         ngrok.set_auth_token(os.getenv("NGROK_AUTH_TOKEN"))
+
+#     public_url = ngrok.connect(PORT).public_url
+#     print(f"Webhook URL: {public_url}/webhook")
+#     uvicorn.run(app, host="0.0.0.0", port=PORT)
 
 if __name__ == "__main__":
-    PORT = 8000
-    if os.getenv("NGROK_AUTH_TOKEN"):
-        ngrok.set_auth_token(os.getenv("NGROK_AUTH_TOKEN"))
-    public_url = ngrok.connect(PORT).public_url
-    print(f"Webhook URL: {public_url}/webhook")
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run(
+        "start:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
